3fa11886151cb240b37d9c532b70a443425f6e1d
DrJul <zsuiluj@o2.pl>
2024-02-04 22:55:03 +0100

    init project

---
+++ src/test/java/com/pl/animalfoodcomparer_be/AnimalFoodComparerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.animalfoodcomparer_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalFoodComparerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
