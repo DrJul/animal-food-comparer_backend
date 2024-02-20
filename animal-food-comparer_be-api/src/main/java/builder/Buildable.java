7963501e7c7f07b3831b98a1670f21340a44f44a
DrJul <zsuiluj@o2.pl>
2024-02-21 00:10:47 +0100

    Builder generator corrected

--- animal-food-comparer_be-api/src/main/java/builder/Buildable.java
+++ animal-food-comparer_be-api/src/main/java/builder/Buildable.java
@@ -5,11 +5,12 @@ import net.karneim.pojobuilder.GeneratePojoBuilder;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation for generating builders in Transfer Objects with OpenApi Generator
+ * https://github.com/mkarneim/pojobuilder
+ */
 @GeneratePojoBuilder(
-    withFactoryMethod = "instance",
-        withSetterNamePattern = "with*",
-        withBuilderInterface = Builder.class,
-        withBuilderProperties = true
+    withFactoryMethod = "instance"
 )
 @Target({ElementType.ANNOTATION_TYPE, ElementType.TYPE})
 public @interface Buildable {
--- animal-food-comparer_be-api/src/main/java/builder/To.java
+++
@@ -1,5 +0,0 @@
-package builder;
-
-@Buildable
-public @interface To {
-}
