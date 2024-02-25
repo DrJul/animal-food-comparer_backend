0c43332c4f13e4d48d132a2a38c5024f3a7a2e84
DrJul <zsuiluj@o2.pl>
2024-02-25 14:33:59 +0100

    Names convention corrected

---
+++ food-comparer_be/src/main/java/com/pl/FoodComparer.java
@@ -0,0 +1,12 @@
+package com.pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodComparer {
+
+    public static void main(String[] args){
+        SpringApplication.run(FoodComparer.class, args);
+    }
+}
